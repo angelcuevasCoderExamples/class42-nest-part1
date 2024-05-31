9a12738fb329750813e014e54e6877444f2ef8db
angelcuevas <angel.p.cuevas@gmail.com>
2024-05-30 23:01:00 -0300

    added: users and products module

---
+++ src/users/users.controller.ts
@@ -0,0 +1,68 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpException,
+  HttpStatus,
+  Query,
+  Request,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('api/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    const { first_name, last_name, email, password, avatar } = createUserDto;
+    if (!first_name || !last_name || !email || !password || !avatar) {
+      throw new HttpException('incomplete data', HttpStatus.BAD_REQUEST);
+    }
+
+    const newUser = this.usersService.create(createUserDto);
+    return { status: 'success', payload: newUser };
+  }
+
+  @Get()
+  findAll(@Query() query) {
+    console.log(query);
+    const users = this.usersService.findAll(query);
+    return { status: 'success', payload: users };
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    if (isNaN(+id)) {
+      throw new HttpException('invalid param', HttpStatus.BAD_REQUEST);
+    }
+    const user = this.usersService.findOne(+id);
+    return { status: 'success', payload: user };
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    const updatedUser = this.usersService.update(+id, updateUserDto);
+    return { status: 'success', payload: updatedUser };
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    
+    return this.usersService.remove(+id);
+  }
+
+  @Post('/test/:id')
+  requestTest(@Request() req) {
+    console.log(req.query);
+    console.log(req.body);
+    console.log(req.params);
+    return 'complete request';
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,58 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  static id: number = 0;
+  users: User[];
+  constructor() {
+    this.users = [];
+  }
+  create(createUserDto: CreateUserDto) {
+    const user = new User();
+    user.id = ++UsersService.id;
+    user.first_name = createUserDto.first_name;
+    user.last_name = createUserDto.last_name;
+    user.email = createUserDto.email;
+    user.password = createUserDto.password;
+    user.avatar = createUserDto.avatar;
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  findAll(query?: any) {
+    if (query && query.limit) {
+      return this.users.slice(0, query.limit);
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const index = this.users.findIndex((u) => u.id === id);
+    if (index == -1) {
+      throw new HttpException('No user by that id', HttpStatus.BAD_REQUEST);
+    }
+    return this.users[index];
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    const user = this.findOne(id);
+    user.first_name = updateUserDto.first_name;
+    user.last_name = updateUserDto.last_name;
+    user.email = updateUserDto.email;
+    user.password = updateUserDto.password;
+    user.avatar = updateUserDto.avatar;
+
+    return user;
+  }
+
+  remove(id: number) {
+    this.findOne(id);
+    const index = this.users.findIndex((u) => u.id === id);
+    return this.users.splice(index, 1);
+  }
+}
